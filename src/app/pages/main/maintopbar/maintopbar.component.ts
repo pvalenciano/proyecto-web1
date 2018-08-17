debe66604fc6af2768119f2a83feb7fa1338e09a
paula <p.valenciano.sausor@accenture.com>
2018-08-17 13:39:36 +0200

    add auth status

--- src/app/pages/main/maintopbar/maintopbar.component.ts
+++ src/app/pages/main/maintopbar/maintopbar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit,ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router, ActivatedRoute, RouterModule, Routes } from '@angular/router';
+import { AuthService } from '../../../services/index';
 
 @Component({
   selector: 'app-maintopbar',
@@ -10,10 +11,32 @@ import { Router, ActivatedRoute, RouterModule, Routes } from '@angular/router';
 })
 export class MaintopbarComponent implements OnInit {
 
-  constructor( public router: Router,
-    public route: ActivatedRoute) { }
+  public isLogin:boolean;
+  public userName:string;
+  public userEmail:string;
+  
+  constructor(
+    public router: Router,
+    public route: ActivatedRoute,
+    public authService: AuthService
+  ) { }
 
   ngOnInit() {
+    this.authService.getAuth().subscribe(auth => {
+      if (auth) {
+        this.isLogin = true;
+        this.userName = auth.displayName;
+        this.userEmail = auth.email;
+
+
+      } else {
+        this.isLogin = false;
+      }
+    })
+  }
+
+  public onClickLogout() {
+    this.authService.logout();
   }
 
 }
